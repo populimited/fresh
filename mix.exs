e384d9618e18603f0a2832243747e0ba702a7bff
Alessandro Gessa <alessandro@gessa.net>
2026-06-22 14:51:14 +0200

    Support for Elixir 1.19+

--- mix.exs
+++ mix.exs
@@ -47,8 +47,8 @@ defmodule Fresh.MixProject do
     ]
   end
 
-  defp elixirc_paths(:test), do: [~c"lib", ~c"test/support"]
-  defp elixirc_paths(_), do: [~c"lib"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   def application do
     [
